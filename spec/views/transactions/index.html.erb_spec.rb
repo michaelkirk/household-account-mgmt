e83ee8cf9291df9e6efd4e94d58afe117e3eeb8a
Michael Kirk <michael.john.kirk@gmail.com>
2011-01-17 15:58:22 -0800

    modified index spec to reflect index

--- spec/views/transactions/index.html.erb_spec.rb
+++ spec/views/transactions/index.html.erb_spec.rb
@@ -23,10 +23,8 @@ describe "transactions/index.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "9.99".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select "tr>td", :text => "debit".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Message".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
   end
 end
