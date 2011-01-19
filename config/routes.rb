8b0ed9d6ccdb04fdb8e69f4bcd17861a485a89df
Michael Kirk <michael.john.kirk@gmail.com>
2011-01-19 10:44:46 -0800

    changed root to household listing

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Foodlobby::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "households#index"
 
   # See how all your routes lay out with "rake routes"
 
