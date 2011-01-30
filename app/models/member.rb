9c85436849fc299c23634df49293a3d2ef2bc8c3
Michael Kirk <michael.john.kirk@gmail.com>
2011-01-30 13:07:29 -0600

    print member email or "unknown" when name missing

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,14 +2,22 @@ class Member < ActiveRecord::Base
   belongs_to :household
 
   before_validation do |member|
+    # a member must always belong to a household. 
+    # If they aren't currently in a household, make a new one for them.
     member.build_household if member.household.nil?
   end
 
   def to_s
-    name
+    if name != ''
+      name
+    elsif email.to_s != ''
+      email.to_s
+    else
+      '(unknown member)'
+    end
   end
 
   def name
-    "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}".strip
   end
 end
