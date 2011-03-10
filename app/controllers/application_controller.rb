9d6d127119efb1ce5fa4ba9eb8525cd05163fbe0
Michael Kirk <michael.john.kirk@gmail.com>
2011-03-09 23:46:23 -0800

    changing to PDT timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  # Why do we have to specify this here AND in initializers/time_zone.rb?
+  Time.zone="Pacific Time (US & Canada)"
 end
