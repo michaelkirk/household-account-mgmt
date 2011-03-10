5b83fc2a839b615eb915ae067c21df900ad72ad8
Michael Kirk <michael.john.kirk@gmail.com>
2011-03-09 20:03:23 -0800

    monotonic improvements on passing specs

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -4,11 +4,15 @@ describe "Transactions" do
   before do
     @household = stub_model(Household, {:id => 1})
     Household.create!(:id=>1)
+    def current_user
+      mock_model(User)
+    end
   end
+
   describe "GET /transactions" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get household_transactions_path(@household)
+      get all_transactions_url
       response.status.should be(200)
     end
   end
--- spec/views/transactions/show.html.erb_spec.rb
+++ spec/views/transactions/show.html.erb_spec.rb
@@ -17,7 +17,7 @@ describe "transactions/show.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/9.99/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/false/)
+    rendered.should match(/debit/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Message/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
