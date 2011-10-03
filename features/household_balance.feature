528a4c5d91bf4daf4b17c9e946d089a82c7cb9b5
Michael Kirk <michael.john.kirk@gmail.com>
2011-10-02 21:55:00 -0700

    household balance cuke

--- features/household_balance.feature
+++ features/household_balance.feature
@@ -1,9 +1,13 @@
 Feature: Household balance
-  In order to
-  As an
-  I want
 
-  Scenario:
-    Given
-    When
-    Then
+  In order to know if a member needs to make an investment
+  As a bulkroom worker
+  I want to see the member's household's balance
+
+  Background: Logged in
+    Given I am logged in
+
+  Scenario: View a household balance
+    Given a household with a $50 balance
+    When I view that household
+    Then I should see "Balance: $50.00"
--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,7 +1,16 @@
+Given /^I am logged in$/ do
+  Given "I am not logged in"
+  Given 'a user with email "somebody@foodlobby.org" and password "secret"'
+  And "I am on the sign in page"
+  When 'I fill in "somebody@foodlobby.org" for "Email"'
+  And 'I fill in "secret" for "Password"'
+  And 'I press "Sign in"'
+end
+
 Given /^I am not logged in$/ do
   #not logged in by default, so do nothing
 end
 
 Given /^a user with email "([^"]*)" and password "([^"]*)"$/ do |email, password|
-  User.create(:email => email, :password => password)
+  User.create!(:email => email, :password => password)
 end
