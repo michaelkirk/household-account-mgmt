1250cca104716faaa0306047a3280b06a54c7f23
Michael Kirk <michael.john.kirk@gmail.com>
2012-02-27 13:26:58 -0800

    put revision in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :set_revision_url
+
   # Why do we have to specify this here AND in initializers/time_zone.rb?
   Time.zone="Pacific Time (US & Canada)"
+
+  def set_revision_url
+    @revision_url = 'https://github.com/michaelkirk/household-account-mgmt/'
+    @revision += "tree/#{ENV['commit_hash']}" unless ENV['commit_hash'].nil?
+  end
 end
