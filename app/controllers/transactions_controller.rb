e9c9781d34e51c9cbedd239da440910de8f577bf
Fernando Marcheselli <fmarcheselli@gmail.com>
2012-12-03 15:22:39 -0800

    Reformated Table

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,13 @@
 class TransactionsController < ApplicationController
+  before_filter :except => :all_households do
+     @household = Household.find(params[:household_id])
+  end
 
   def all_households
-    @transactions = Transaction.all(:include => {:household => [:members]})
+    @transactions = Transaction.includes(:household => [:members]).order("id DESC").paginate(:page => params[:page], :per_page => 30)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @transactions }
       format.csv  { render :csv => @transactions }
     end
   end
@@ -13,8 +15,7 @@ class TransactionsController < ApplicationController
   # GET /households/:id/transactions
   # GET /households/:id/transactions.xml
   def index
-    @household = Household.find(params[:household_id])
-    @transactions = Transaction.for_household(params[:household_id]).all
+     @transactions = Transaction.for_household(params[:household_id]).all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -25,7 +26,6 @@ class TransactionsController < ApplicationController
   # GET /households/:id/transactions/1
   # GET /households/:id/transactions/1.xml
   def show
-    @household = Household.find(params[:household_id])
     @transaction = Transaction.for_household(params[:household_id]).find(params[:id])
 
     respond_to do |format|
@@ -37,7 +37,6 @@ class TransactionsController < ApplicationController
   # GET /households/:id/transactions/new
   # GET /households/:id/transactions/new.xml
   def new
-    @household = Household.find(params[:household_id])
     @transaction = Transaction.for_household(params[:household_id]).new
     respond_to do |format|
       format.html # new.html.erb
@@ -47,14 +46,12 @@ class TransactionsController < ApplicationController
 
   # GET /households/:id/transactions/1/edit
   def edit
-    @household = Household.find(params[:household_id])
     @transaction = Transaction.for_household(params[:household_id]).find(params[:id])
   end
 
   # POST /households/:id/transactions
   # POST /households/:id/transactions.xml
   def create
-    @household = Household.find(params[:household_id])
     @transaction = Transaction.for_household(params[:household_id]).new(params[:transaction])
 
     respond_to do |format|
@@ -71,7 +68,6 @@ class TransactionsController < ApplicationController
   # PUT /households/:id/transactions/1
   # PUT /households/:id/transactions/1.xml
   def update
-    @household = Household.find(params[:household_id])
     @transaction = Transaction.for_household(params[:household_id]).find(params[:id])
 
     respond_to do |format|
@@ -89,7 +85,6 @@ class TransactionsController < ApplicationController
   # DELETE /households/:id/transactions/1.xml
   def destroy
     raise Exception.new("deleting transactions is not allowed")
-    @household = Household.find(params[:household_id])
     @transaction = Transaction.for_household(params[:household_id]).find(params[:id])
     @transaction.destroy
 
