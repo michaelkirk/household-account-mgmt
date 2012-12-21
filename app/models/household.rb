592334e1bddede0ef0d34af9a153b205a9512ba1
Michael Kirk <michael.john.kirk@gmail.com>
2012-12-21 13:36:08 -0600

    app doesn't explode when searching for inactive household

--- app/models/household.rb
+++ app/models/household.rb
@@ -4,17 +4,21 @@ class Household < ActiveRecord::Base
   has_many :household_membership_audits
 
   #OPTIMIZE this horse shit.
-  def self.find_by_keywords(words)
+  def self.id_by_keywords(words)
     return self.all if(words.strip.empty?)
 
     words.split.inject(Set.new()) do |members, word|
       #Get all members who's first or last name matches
       members + Member.where('UPPER(first_name) LIKE UPPER(?) OR UPPER(last_name) LIKE UPPER(?)' , "%#{word}%", "%#{word}%")
     end.map do |member|
-      #Get their households
-      member.household
+      #Get their household ids
+      member.household_id
     end.uniq #remove dupes.
   end
+
+  scope :find_by_keywords, lambda { |words| 
+    where("households.id in (?)", id_by_keywords(words)) 
+  }
 
   # All households with no members
   scope :empty, joins('left outer join members on members.household_id = households.id').select('households.*').where('members.id is null')
